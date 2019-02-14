aa46f1b1a1589e3ac6873efde3462654a0c68c76
Joris Rietveld <jorisrietveld@gmail.com>
2019-02-14 15:33:09 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,6 @@
 # Author: Joris Rietveld <jorisrietveld@gmail.com>
 # Created: 20-08-2017 13:52
 # Licence: GPLv3 - General Public Licence version 3
-
 from setuptools import setup, find_packages
 
 
